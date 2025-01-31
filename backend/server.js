9ddc979d1a9c69457c489ee968e612c9ae057c3d
ArielGana <ganaariel5@gmail.com>
2025-01-31 18:07:31 -0300

    primera commit

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,3 @@ app.get("/", (req, res) => {
 });
 
 // Iniciar el servidor
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
