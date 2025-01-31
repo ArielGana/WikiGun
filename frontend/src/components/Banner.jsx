b6bed2c7d1af494a980739ecef9f60d5116a9b7a
ArielGana <ganaariel5@gmail.com>
2025-01-31 18:37:48 -0300

    primera commit

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -9,7 +9,7 @@ const Banner = () => {
   const fetchImages = async () => {
     try {
       const response = await axios.get(
-        "http://localhost:5000/api/cloudinary/images"
+        "https://wiki-gun.vercel.app//api/cloudinary/images"
       );
       setImages(response.data);
     } catch (error) {
